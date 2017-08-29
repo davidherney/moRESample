7d92ec461eef65d8e1634e50b718d17884f374ec
David Herney <davidherney@gmail.com>
2017-08-29 11:59:24 -0500

    Add basic index file

--- index_base.php
+++ index_base.php
@@ -1,151 +1,9 @@
 <?php
 
-define('_DEBUG', false);
+define('_DEBUG', true);
 
 $errors = array();
 $msgs = array();
-$url = $token = '';
-$courseid = null;
-$users = null;
-$activities = null;
-if (isset($_POST['execute_query']) || isset($_POST['execute_grade'])) {
-
-    if (empty($_POST['inputURL'])) {
-        $errors[] = 'La URL de la plataforma es requerida';
-    }
-    else {
-        $url = $_POST['inputURL'];
-    }
-
-    if (empty($_POST['inputToken'])) {
-        $errors[] = 'El token es requerido';
-    }
-    else {
-        $token = $_POST['inputToken'];
-    }
-
-    if (empty($_POST['inputCourse'])) {
-        $errors[] = 'El id del curso es requerido';
-    }
-    else {
-        $courseid = $_POST['inputCourse'];
-    }
-
-    if (count($errors) == 0) {
-        query_activities();
-        query_users();
-
-        if (isset($_POST['execute_grade'])) {
-            set_grades();
-        }
-    }
-
-}
-
-function query_users() {
-    global $url, $token, $errors, $courseid, $users;
-
-    $params = array(
-        'wstoken' => $token,
-        'wsfunction' => 'core_enrol_get_enrolled_users',
-        'moodlewsrestformat' => 'json',
-        'courseid' => $courseid,
-        'options' => array(
-                        array('name' => 'withcapability', 'value' => 'moodle/grade:view'),
-                        array('name' => 'onlyactive', 'value' => 1)
-                    )
-    );
-
-    $users = curl_post($url, $params);
-
-    if ($users === false) {
-        return false;
-    }
-
-    if (empty($users)) {
-        $errors[] = 'No se encontraron estudiantes matriculados en el curso';
-    }
-
-}
-
-function query_activities() {
-    global $url, $token, $errors, $courseid, $activities;
-
-    $params = array(
-        'wstoken' => $token,
-        'wsfunction' => 'core_course_get_contents',
-        'moodlewsrestformat' => 'json',
-        'courseid' => $courseid,
-        'options' => array(
-                        array('name' => 'excludecontents', 'value' => true)
-                    )
-    );
-
-    $sections = curl_post($url, $params);
-
-    if ($sections === false) {
-        return false;
-    }
-
-    if (empty($sections)) {
-        $errors[] = 'No se encontraron actividades en el curso';
-    }
-
-    $activities = array();
-    foreach($sections as $section) {
-        $activities += $section->modules;
-    }
-
-    if (count($activities) == 0) {
-        $activities = null;
-        $errors[] = 'No se encontraron actividades en el curso';
-    }
-
-}
-
-function set_grades() {
-    global $url, $token, $errors, $msgs, $courseid;
-
-    $grades = array();
-
-    foreach($_POST['inputUser'] as $key => $grade) {
-        if (!empty($grade)) {
-            $grades[] = array('studentid' => $key, 'grade' => $grade, 'str_feedback' => 'Asignado desde moRESample');
-        }
-    }
-
-    if (count($grades) == 0) {
-        $errors[] = 'No se reportaron calificaciones para asignar.';
-        return;
-    }
-
-    $act = explode('|', $_POST['selectActivity']);
-
-    $params = array(
-        'wstoken' => $token,
-        'wsfunction' => 'core_grades_update_grades',
-        'moodlewsrestformat' => 'json',
-        'source' => 'moRESample',
-        'courseid' => $courseid,
-        'component' => $act[0],
-        'activityid' => $act[1],
-        'itemnumber' => 0,
-        'grades' => $grades
-    );
-
-    $res = curl_post($url, $params);
-
-    if ($res === false) {
-        return false;
-    }
-
-//    if ($res) {
-        $msgs[] = 'Calificaciones guardadas con éxito';
-//    }
-//    else {
-//        $errors[] = 'No se pudieron almacenar las calificaciones';
-//    }
-}
 
 /**
  * Send a POST requst using cURL
@@ -159,11 +17,11 @@ function set_grades() {
  */
 function curl_post($url, array $post = NULL, array $options = array()) {
     global $errors;
-    $l_url = trim($url, '/') . '/webservice/rest/server.php';
+
     $defaults = array(
         CURLOPT_POST => 1,
         CURLOPT_HEADER => 0,
-        CURLOPT_URL => $l_url,
+        CURLOPT_URL => $url,
         CURLOPT_FRESH_CONNECT => 1,
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_FORBID_REUSE => 1,
@@ -271,13 +129,13 @@ function curl_post($url, array $post = NULL, array $options = array()) {
                     <div class="form-group">
                         <label for="inputURL" class="col-lg-2 control-label">URL de la plataforma</label>
                         <div class="col-lg-10">
-                            <input class="form-control" id="inputURL" name="inputURL" placeholder="https://dominio/plataforma/" type="text" value="<? echo $url; ?>" />
+                            <input class="form-control" id="inputURL" name="inputURL" placeholder="https://dominio/plataforma/" type="text" />
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="inputToken" class="col-lg-2 control-label">Token</label>
                         <div class="col-lg-10">
-                            <input class="form-control" id="inputToken" name="inputToken" placeholder="Token" type="text" value="<? echo $token; ?>" />
+                            <input class="form-control" id="inputToken" name="inputToken" placeholder="Token" type="text" />
                         </div>
                     </div>
                 </fieldset>
@@ -287,7 +145,7 @@ function curl_post($url, array $post = NULL, array $options = array()) {
                     <div class="form-group">
                         <label for="inputCourse" class="col-lg-2 control-label">Id del curso</label>
                         <div class="col-lg-10">
-                            <input class="form-control" id="inputCourse" name="inputCourse" placeholder="##" type="text" value="<? echo $courseid; ?>" />
+                            <input class="form-control" id="inputCourse" name="inputCourse" placeholder="##" type="text" />
                         </div>
                     </div>
                     <div class="col-lg-10 col-lg-offset-2">
@@ -298,73 +156,41 @@ function curl_post($url, array $post = NULL, array $options = array()) {
             </div>
         </div>
 
-<?php
-    if ($users || $activities) {
-?>
         <div class="row">
             <div class="col">
                 <form class="form-horizontal" method="POST">
                 <fieldset>
                     <legend>Actividad</legend>
-<?php
-    if ($activities) {
-?>
                     <div class="form-group">
                         <label for="inputActivity" class="col-lg-2 control-label">Actividad</label>
                         <div class="col-lg-10">
                             <select class="form-control" id="selectActivity" name="selectActivity">
-<?php
-        foreach ($activities as $activity) {
-?>
-                            <option value="mod_<? echo $activity->modname . '|' . $activity->id; ?>"><? echo $activity->name . ' (' . $activity->modplural . ')'; ?></option>
-<?php
-        }
-?>
+                            <option value="mod_forum">Un foro</option>
                             </select>
                         </div>
                     </div>
-<?php
-    }
-?>
                 </fieldset>
 
                 <fieldset>
                     <legend>Estudiantes</legend>
-<?php
-    if ($users) {
-        foreach ($users as $user) {
-
-?>
                     <div class="form-group">
-                        <label for="inputUser_<? echo $user->id; ?>" class="col-lg-2 control-label"><? echo $user->fullname . (empty($user->idnumber) ? '' : ' (#' . $user->idnumber . ')'); ?></label>
+                        <label for="inputUser" class="col-lg-2 control-label">[Nombre del usuario]</label>
                         <div class="col-lg-10">
-                            <input class="form-control" id="inputUser_<? echo $user->id; ?>" name="inputUser[<? echo $user->id; ?>]" placeholder="##" type="text" />
+                            <input class="form-control" id="inputUser" name="inputUser" placeholder="##" type="text" />
                         </div>
                     </div>
-<?php
-        }
-    }
-
-    if ($users && $activities) {
-?>
                     <div class="col-lg-10 col-lg-offset-2">
                         <button type="reset" class="btn btn-default">Limpiar</button>
                         <button type="submit" class="btn btn-primary" name="execute_grade">Asignar calificaciones</button>
                     </div>
-<?php
-    }
-?>
                 </fieldset>
 
-                    <input name="inputURL" type="hidden" value="<? echo $url; ?>" />
-                    <input name="inputToken" type="hidden" value="<? echo $token; ?>" />
-                    <input name="inputCourse" type="hidden" value="<? echo $courseid; ?>" />
+                    <input name="inputURL" type="hidden" value="" />
+                    <input name="inputToken" type="hidden" value="" />
+                    <input name="inputCourse" type="hidden" value="" />
                 </form>
             </div>
         </div>
-<?php
-}
-?>
     </div>
 
     <script src="js/bootstrap.min.js"></script>
